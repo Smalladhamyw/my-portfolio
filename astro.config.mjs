8df191c46057a910afb48fecf009e97d1bfd9c1f
Smalladhamyw <adham.dweck10@gmail.com>
2026-07-17 23:33:37 -0400

    fix: temporary disable sitemap to fix build crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap'; // Remove or comment this out
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://Smalladhamyw.github.io',
   base: '/my-portfolio',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    mdx(), 
+    // sitemap(),                      // Remove or comment this out
+    tailwind()
+  ],
 });
