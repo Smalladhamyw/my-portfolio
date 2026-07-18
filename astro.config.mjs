46475943e8c3ae8212a086d283d4d517edbdade7
Smalladhamyw <adham.dweck10@gmail.com>
2026-07-17 23:12:46 -0400

    Update config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://Smalladhamyw.github.io',
+  base: '/my-portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
